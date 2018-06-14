49009fd4e9dadaceedac0c469bca77b1a2258633
baksteen-13 <nielsvanbrenk@gmail.com>
2018-06-14 11:41:17 +0200

    de kaasjes

---
+++ app/src/main/java/com/example/baksteen_13/dinopackopening/loadscreen.java
@@ -0,0 +1,49 @@
+package com.example.baksteen_13.dinopackopening;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class loadscreen extends AppCompatActivity {
+
+    private ProgressBar mLoadbar;
+
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loadscreen);
+
+        mLoadbar = (ProgressBar) findViewById(R.id.Loadbar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(mProgressStatus < 100){
+                    mProgressStatus++;
+                    android.os.SystemClock.sleep(50);
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mLoadbar.setProgress(mProgressStatus);
+                        }
+                    });
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(loadscreen.this, MainActivity.class));
+
+                    }
+                });
+            }
+        }).start();
+    }
+}
