3ce79063984148c981304707a15f91c1d0117a66
Thomas Nolst Trenite <Thomas@MacBook-Pro-van-Thomas.local>
2018-06-17 23:04:34 +0200

    kaas

--- app/src/main/java/com/example/baksteen_13/dinopackopening/MainActivity.java
+++ app/src/main/java/com/example/baksteen_13/dinopackopening/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.baksteen_13.dinopackopening;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +9,8 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,9 +20,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    String description = "Android Developement Tutorial";
+    EditText editText;
+    Button btn;
+
     private TextView mTextMessage;
-
-
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private int i;
@@ -48,6 +53,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String namevalue = editText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("NAME", namevalue);
+                intent.putExtra("DISCRIPTION", description);
+                startActivity(intent);
+            }
+        });
+
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         setContentView(R.layout.activity_main);
