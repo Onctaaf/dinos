83d43ef0400c20f2179fb523545b188adb507850
Thomas Nolst Trenite <Thomas@MacBook-Pro-van-Thomas.local>
2018-06-18 22:23:32 +0200

    de kaasjes

---
+++ app/src/main/java/com/example/baksteen_13/dinopackopening/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.baksteen_13.dinopackopening;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView nameView=(TextView) findViewById(R.id.result_txt);
+        nameView.setText(getIntent().getExtras().getString("username"));
+
+        configureBackButton();
+    }
+
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/baksteen_13/dinopackopening/ThirdFragment.java
+++ app/src/main/java/com/example/baksteen_13/dinopackopening/ThirdFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +17,32 @@ public class ThirdFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.third_layout, container, false);
+
+        final EditText et = (EditText) myView.findViewById(R.id.userText);
+
+        Button nextButton = (Button) myView.findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SecondActivity.class);
+                intent.putExtra("username", et.getText().toString());
+                Bundle b = new Bundle();
+                b.putInt("key", 1);
+                intent.putExtras(b);
+                startActivity(intent);
+
+                /*Snackbar snackbar = Snackbar
+                        .make(v, "Welcome to AndroidHive", Snackbar.LENGTH_LONG);
+
+                snackbar.show();*/
+            }
+        });
+
         return myView;
 
-    }
+
+    } /*hier stopt de on create*/
+
+
 
 }
